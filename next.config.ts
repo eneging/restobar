a0598b0f08d873e825034d00bc62e07271c4e490
eneging <140635337+eneging@users.noreply.github.com>
2026-01-22 09:35:33 -0500

    restobard

--- next.config.ts
+++ next.config.ts
@@ -17,23 +17,7 @@ const nextConfig: NextConfig = {
 
   env: {
     // ✅ ESTE ES EL QUE USA TODO TU FRONT
-    NEXT_PUBLIC_API_URL:"https://licorapi.puertoricoica.online/api",
 
-    NEXT_PUBLIC_MP_PUBLIC_KEY:
-      "APP_USR-ba1bb53f-827c-4c39-9e25-7716c4e57455",
-    NEXT_PUBLIC_CULQI_PUBLIC_KEY:
-      "pk_test_h6x1GXjwAYCK2fLD",
-
-    NEXTAUTH_URL: "https://licorapi.puertoricoica.online/api",
-    NEXTAUTH_SECRET: "tu_secreto_super_seguro_generalo_con_openssl",
-
-    GOOGLE_CLIENT_ID:
-      "702949617967-8oudu9l2hdkq6laqps0u23lc6i2oa0r6.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET:
-      "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",
-
-    FACEBOOK_CLIENT_ID: "tuappid",
-    FACEBOOK_CLIENT_SECRET: "tusecreto",
   },
 };
 
